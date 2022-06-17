7298fb20c0def8bcd3ac5d17173f503a598dfb1e
Ryzen9-5950X-RTX3090 <jordanluders@gmail.com>
2022-06-17 06:21:02 -0500

    Snake for Linux.

---
+++ Snake-LinuxUnitySourceCode/Snake/Assets/Scripts/Food.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public Collider2D gridArea;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    public void RandomizePosition()
+    {
+        Bounds bounds = gridArea.bounds;
+
+        // Pick a random position inside the bounds
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        // Round the values to ensure it aligns with the grid
+        x = Mathf.Round(x);
+        y = Mathf.Round(y);
+
+        transform.position = new Vector2(x, y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        RandomizePosition();
+    }
+
+}
