ea67d9f39c91c9fccdc1f3228eaf6c6ceeea49a6
Ryzen9-5950X-RTX3090 <jordanluders@gmail.com>
2022-07-10 08:22:02 -0500

    Snake v2.0.0

---
+++ Snake-LinuxUnitySourceCode/Snake Arcade/Assets/Scripts/SnakeClassicEvenBetterMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SnakeClassicEvenBetterMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Click () {
+		SceneManager.LoadScene ("SnakeClassicEvenBetter");
+	}
+
+	// Update is called once per frame
+
+}
---
+++ Snake-LinuxUnitySourceCode/Snake Arcade/Assets/Scripts/SnakeClassicMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SnakeClassicMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Click () {
+		SceneManager.LoadScene ("SnakeClassic");
+	}
+
+	// Update is called once per frame
+
+}
