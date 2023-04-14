4726448a0c02abbd7116b6811cb9bd270f88c946
Ryzen5-3600 <jordanluders@proton.me>
2023-04-14 06:49:25 -0500

    Organize.

---
+++ Linux programs and such/Digital Clock V1.0.0/DigitalClock.bash
@@ -0,0 +1,16 @@
+#1/bin/bash
+
+while true
+do
+        clear
+        echo By: Ryzen5-3600
+        echo
+        echo Last updated on: April 13, 2023
+        echo
+        echo Version: 1.0.0
+        echo
+        echo View my other projects at: https://github.com/R5-3600
+        echo
+        echo $(date +%T)
+        sleep 1s
+done
