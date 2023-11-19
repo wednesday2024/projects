cd38a288d6405cef12e724bfae480a7dd9463e84
Ryzen5-3600 <jordanluders@proton.me>
2023-11-18 20:33:23 -0600

    Digital Clock V2

---
+++ Linux programs and such/Digital Clock V2.0.0/DigitalClock.bash
@@ -0,0 +1,16 @@
+#1/bin/bash
+
+while true
+do
+        clear
+        echo By: Ryzen5-3600
+        echo
+        echo Last updated on: November 18, 2023
+        echo
+        echo Version: 2.0.0
+        echo
+        echo View my other projects at: https://github.com/R5-3600
+        echo
+        echo Your local time is: $(date +%r)
+        sleep 1s
+done
